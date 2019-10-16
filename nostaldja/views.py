97c2c6f08fc064a59c3d1062787b7dbd27656f3b
tiffanymesser <tiffanymesser15@gmail.com>
2019-10-16 15:17:08 -0400

    Added settings.sql

--- nostaldja/views.py
+++ nostaldja/views.py
@@ -6,6 +6,7 @@ class DecadeList(generics.ListCreateAPIView):
     queryset = Decade.objects.all()
     serializer_class = DecadeSerializer
 
+
 class DecadeDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Decade.objects.all()
     serializer_class = DecadeSerializer
---
+++ settings.sql
@@ -0,0 +1,7 @@
+
+CREATE DATABASE nostaldja;
+CREATE USER nostaldjauser WITH PASSWORD 'nostaldja';
+GRANT ALL PRIVILEGES ON DATABASE nostaldja TO nostaldjauser;
+
+
+

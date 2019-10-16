67a6168e0afab41d9def0b373bf9089e7ed5cbdb
Walter_Porzel <walterp@vt.edu>
2019-10-16 16:50:33 -0400

    added cors allow all

--- nostaldja_django/settings.py
+++ nostaldja_django/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'nostaldja_django.urls'
 
